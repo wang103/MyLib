3458bfc5d872bede242ecc730fed3526582f299d
Tianyi Wang <wang103uiuc@gmail.com>
2012-11-09 23:22:00 -0600

    Heapify up and down finished

--- PriorityQueue/PriorityQueue.cpp
+++ PriorityQueue/PriorityQueue.cpp
@@ -1,27 +1,79 @@
-template <class T>
-PriorityQueue<T>::PriorityQueue() {
+/**
+ * Assume that the binary tree rooted at l_child(i) and r_child(i) are heaps,
+ * the ith element might be violating the heap property by having priority less
+ * than at least one of its children. This function corrects such situation so
+ * that the tree rooted at i becomes a heap.
+ */
+template <class T, class Compare>
+void PriorityQueue<T, Compare>::heapifyDown(int i) {
+    int lchildIndex = l_child(i);
+    int rchildIndex = r_child(i);
+    int hpIndex = i;
+
+    if (lchildIndex < queue.size() &&
+            comp(queue[lchildIndex], queue[hpIndex])) {
+        hpIndex = lchildIndex;
+    }
+    if (rchildIndex < queue.size() &&
+            comp(queue[rchildIndex], queue[hpIndex])) {
+        hpIndex = rchildIndex;
+    }
+
+    if (hpIndex != i) {
+        T temp = queue[i];
+        queue[i] = queue[hpIndex];
+        queue[hpIndex] = temp;
+
+        heapifyDown(hpIndex);
+    }
+}
+
+/**
+ * Assume that the entire binary tree is a heap except the ith element, the ith
+ * element might be violating the heap property by having priority higher than
+ * its parent. This function corrects such situation so that the entire tree
+ * becomes a heap.
+ */
+template <class T, class Compare>
+void PriorityQueue<T, Compare>::heapifyUp(int i) {
+    int pIndex = parent(i);
+
+    if (comp(queue[i], queue[pIndex])) {
+        T temp = queue[i];
+        queue[i] = queue[pIndex];
+        queue[pIndex] = temp;
+
+        heapifyUp(pIndex);
+    }
+}
+
+template <class T, class Compare>
+PriorityQueue<T, Compare>::PriorityQueue() {
     //TODO: implementation.
 }
 
-template <class T>
-bool PriorityQueue<T>::isEmpty() const {
+template <class T, class Compare>
+PriorityQueue<T, Compare>::PriorityQueue(vector<T> elements) {
     //TODO: implementation.
-    return true;
 }
 
-template <class T>
-int PriorityQueue<T>::size() const {
-    //TODO: implementation.
-    return 0;
+template <class T, class Compare>
+bool PriorityQueue<T, Compare>::isEmpty() const {
+    return queue.empty();
 }
 
-template <class T>
-const T& PriorityQueue<T>::extractMin() {
+template <class T, class Compare>
+int PriorityQueue<T, Compare>::size() const {
+    return queue.size();
+}
+
+template <class T, class Compare>
+const T& PriorityQueue<T, Compare>::popTop() {
     //TODO: implementation.
     return queue[0];
 }
 
-template <class T>
-void push(const T& element) {
+template <class T, class Compare>
+void PriorityQueue<T, Compare>::push(const T& element) {
     //TODO: implementation.
 }
--- PriorityQueue/PriorityQueue.h
+++ PriorityQueue/PriorityQueue.h
@@ -2,19 +2,36 @@
 #define PRIORITY_QUEUE_H
 
 #include <vector>
+#include <functional>
 
 using namespace std;
 
-template <class T>
+// Macro for indexing.
+#define parent(i) (i >> 1)
+#define l_child(i) (i << 1)
+#define r_child(i) (i << 1 & 1)
+
+/**
+ * T: Type of the elements.
+ * Compare: Comparison class -- A class such that the expression comp(a, b),
+ * where comp is an object of this class and a and b are of type T, returns true
+ * if a is said to have higher priority than b. The default is less<T>.
+ */
+template <class T, class Compare = less<T> >
 class PriorityQueue {
 private:
     vector<T> queue;
+    Compare comp;
+
+    void heapifyDown(int i);
+    void heapifyUp(int i);
 
 public:
     PriorityQueue();
+    PriorityQueue(vector<T> elements);
     bool isEmpty() const;
     int size() const;
-    const T& extractMin();
+    const T& popTop();
     void push(const T& element);
 };
 
